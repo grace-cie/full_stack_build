66ab535f249ebcebfdeed8709d6c65b3fe37a5e4
Rirei <reydilipogi@gmail.com>
2022-07-15 16:51:45 +0800

    styled

---
+++ client/src/pages/login/Login.jsx
@@ -0,0 +1,62 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import './login.css'
+
+const Login = ({ title }) => {
+  const [data, setData] = useState('');
+
+  const handleChange = (e) => {
+    const name = e.target.value;
+    const value = e.target.value;
+
+    setData((values) => ({ ...values, [name]: value }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div className=''>
+      <div className='container'>
+          <div className=''>
+            <div className='login-panel panel panel-default'>
+              <div className="panel-heading">
+                <h3 className='panel-title'>{title}</h3>
+              </div>
+              <form className='login-form'>
+                <div className="mb-3">
+                <label className="form-label">Username</label>
+                <input
+                  className='form-control'
+                  type='text'
+                  name='name'
+                  placeholder='Enter your name'
+                  onChange={handleChange}
+                  value={data.userName}
+                />
+                <label className='form-label'>Password</label>
+                <input
+                className='form-control'
+                  type='password'
+                  name='password'
+                  placeholder='Enter your password'
+                  onChange={handleChange}
+                  value={data.password}
+                />
+                <br />
+                <button className='btn btn-primary' onSubmit={handleSubmit}>Login</button>
+                </div>
+              </form>
+                <p>
+                  dont have an account?
+                  <Link to='/signup'> Sign Up Here</Link>
+                </p>   
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
---
+++ client/src/pages/sign-up/Signup.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './sign-up.css'
+
+const Signup = ({title}) => {
+  return (
+    <div className='container'>
+      <div className='panel panel-default'>
+        <div className="panel-heading">
+          <h3 className='panel-title'>{title}</h3>
+        </div>
+        <form>
+          <label className='form-label'>Name</label>
+          <input className='form-control' type='text' name='' id='' placeholder='enter your name' />
+          <label className='form-label'>Username</label>
+          <input className='form-control' type='text' name='' id='' placeholder='enter your username' />
+          <label className='form-label'>Email</label>
+          <input className='form-control' type='text' name='' id='' placeholder='enter your email' />
+          <label className='form-label'>Password</label>
+          <input className='form-control' type='text' name='' id='' placeholder='enter your password' />
+          <label className='form-label'>Confirm password</label>
+          <input
+            className='form-control'
+            type='text'
+            name=''
+            id=''
+            placeholder='enter your confirm password'
+          />
+          <br />
+          <button className='btn btn-primary'>Sign Up</button>
+        </form>
+        <div>
+          <p>
+            Have an account? <Link to='/login'>Login Here!</Link>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Signup;
