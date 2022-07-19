0ecf2085661e4ec6406909799dc2829fbc7e476e
Rirei <reydilipogi@gmail.com>
2022-07-19 22:11:45 +0800

    Book Details Styling

--- client/src/components/books/Books.jsx
+++ client/src/components/books/Books.jsx
@@ -1,19 +1,26 @@
 import React from 'react';
 import './books.css';
+import bookimg from '../../assets/bookimg.jpg'
 
 const Books = ({ books }) => {
   return (
-    <div>
+    <div className='d-flex justify-content-center flex-nowrap'>
       {books.map((book) => (
-        <div key={book._id}>
-          {/* <p>{book.authorId}</p> */}
-          <p>{book.bookTitle}</p>
-          <p>{book.publishDate}</p>
-          <div>
-            <p>{book.review}</p>
-          </div>
-          <p>{book.rating}</p>
-          <p>{book.isAvailable}</p>
+        <div className="card" key={book._id}>
+            <image className="card-img-top" src={bookimg} alt="Card image cap" />
+            <div className="card-body">
+              <h5 className="card-title">{book.bookTitle}</h5>
+              <p className="card-text">Some quick example text to build on the book title and make up the bulk of the card's content.</p>
+            </div>
+            <ul className="list-group list-group-flush">
+              <li className="list-group-item">Publish Date: {book.publishDate}</li>
+              <li className="list-group-item">Rating: {book.rating}</li>
+              <li className="list-group-item">Availability: {book.isAvailable}</li>
+            </ul>
+            <div className="card-body d-flex flex-row">
+              <button className='btn btn-primary p-2'>Borrow</button>
+              <button className='btn btn-info p-2'>View Details</button>
+            </div>
         </div>
       ))}
     </div>
