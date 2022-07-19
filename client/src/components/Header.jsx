34cca9e3d881c66d097a0e90239fc54490ee3e58
Jabame <justinetaglucop@gmail.com>
2022-07-20 00:53:53 +0800

    login done

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
-
+import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import image from '../assets/default2.png';
 
+import { FiLogOut } from 'react-icons/fi';
+
 const Header = () => {
+  const { currentUser } = useSelector((state) => state.user);
+
+  const navigate = useNavigate();
+
+  if (!currentUser) return navigate('/login');
   return (
     <div>
       <nav className='navbar navbar-expand-lg navbar-light bg-light'>
@@ -14,20 +22,32 @@ const Header = () => {
         <div className='d-flex justify-content-end' id='navbarSupportedContent'>
           <ul id='nav-items' className='navbar-nav mr-auto'>
             <li className='nav-item active'>
-              <Link className='nav-link' to={'/login'}>
-                Login<span className='sr-only'>(current)</span>
-              </Link>
-            </li>
-
-            <li className='nav-item active'>
-              <Link className='nav-link' to={'/signup'}>
-                Sign Up<span className='sr-only'>(current)</span>
-              </Link>
-            </li>
-            <li className='nav-item active'>
               <Link className='nav-link' to={'/books'}>
                 Available Books<span className='sr-only'>(current)</span>
               </Link>
+            </li>
+
+            {currentUser ? (
+              <p>Hello Welcome {currentUser.name} </p>
+            ) : (
+              <>
+                <li className='nav-item active'>
+                  <Link className='nav-link' to={'/login'}>
+                    Login<span className='sr-only'>(current)</span>
+                  </Link>
+                </li>
+                <li className='nav-item active'>
+                  <Link className='nav-link' to={'/signup'}>
+                    Sign Up<span className='sr-only'>(current)</span>
+                  </Link>
+                </li>
+              </>
+            )}
+            <li className='nav-item active'>
+              <Link lassName='nav-link' to={'/login'}>
+                <FiLogOut fontSize={24} color='white' />
+              </Link>
+              <span className='sr-only'>(current)</span>
             </li>
           </ul>
         </div>
